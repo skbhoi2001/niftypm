670a4eb841c5dfef39ab77a7a4d98cc4e9aa14bc
skbhoi2001 <soumyak.bhoi.2001@gmail.com>
2021-09-06 18:26:37 +0530

    new

---
+++ js/script.js
@@ -0,0 +1,61 @@
+var btn=document.getElementById("btn")
+var btn1=document.getElementById("btn1")
+btn.addEventListener("mouseover",function(){
+  btn.style.color="#02bdad";
+});
+btn.addEventListener("mouseout",function(){
+  btn.style.color="black";
+});
+
+btn2.addEventListener("mouseover",function(){
+  btn2.style.color="white";
+  btn2.style.backgroundColor="#35AF9A"
+});
+btn2.addEventListener("mouseout",function(){
+  btn2.style.backgroundColor="#02bdad"
+});
+
+
+//button-animation
+var anibutton=document.getElementById("anibutton");
+anibutton.addEventListener("mouseover",function(){
+  anibutton.style.backgroundColor="#8E35EE"
+});
+anibutton.addEventListener("mouseout",function(){
+  anibutton.style.backgroundColor="#D5E0DE"
+});
+
+var anibutton1=document.getElementById("anibutton1");
+anibutton1.addEventListener("mouseover",function(){
+  anibutton1.style.backgroundColor="#8E35EE"
+});
+anibutton1.addEventListener("mouseout",function(){
+  anibutton1.style.backgroundColor="#D5E0DE"
+});
+
+var anibutton2=document.getElementById("anibutton2");
+anibutton2.addEventListener("mouseover",function(){
+  anibutton2.style.backgroundColor="#8E35EE"
+});
+anibutton2.addEventListener("mouseout",function(){
+  anibutton2.style.backgroundColor="#D5E0DE"
+});
+
+var anibutton3=document.getElementById("anibutton3");
+anibutton3.addEventListener("mouseover",function(){
+  anibutton3.style.backgroundColor="#8E35EE"
+});
+anibutton3.addEventListener("mouseout",function(){
+  anibutton3.style.backgroundColor="#D5E0DE"
+});
+
+var anibutton4=document.getElementById("anibutton4");
+anibutton4.addEventListener("mouseover",function(){
+  anibutton4.style.backgroundColor="#8E45EE"
+});
+anibutton4.addEventListener("mouseout",function(){
+  anibutton4.style.backgroundColor="#D5E0DE"
+});
+
+
+//pricing
